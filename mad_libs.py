4d883b074f5da8aa11a64838a28581b0f80b40ed
Este-code <67617811+Este-code@users.noreply.github.com>
2021-05-11 12:20:48 +0100

    Add files via upload

---
+++ mad_libs.py
@@ -0,0 +1,17 @@
+name = input("Insert a name: ")
+silly_word = input("Insert a silly word: ")
+number = input("Insert a number: ")
+adjective1 = input("Insert an adjective: ")
+noun = input("insert a noun: ")
+adjective2 = input("Insert an adjective: ")
+realtive = input("Insert a family realtive:")
+adjective3 = input("Insert an adjective: ")
+verb = input("Insert a verb: ")
+adjective4 = input("Insert an adjective: ")
+adjective5 = input("Insert one more adjective: ")
+
+print("Hello my name is astronaut "+name+". I am on my way to planet "+silly_word+".
+      "I will be gone for "+number+" days. I am very "+adjective1+" about the trip but I will miss my "+noun+". 
+      "I have heard that the atmosphere there is "+adjective2+". Luckily my "+realtive+" packed me a jacket to keep me "+adjective3+". 
+      "When I lan on th eplanet I will "+verb+" for joy. I am "+adjective4+" to walk on another planet. 
+      "I could not be more "+adjective5+" for this trip!")
